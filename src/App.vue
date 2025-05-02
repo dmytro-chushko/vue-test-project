689aed470ff1ac2afc90c91bcc13194ef7c2be3a
Dmytro Chushko <dmytro.chushko@gmail.com>
2025-05-03 01:49:29 +0300

    Add v-for

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,12 @@ import HomepageView from './views/HomepageView.vue'
 
 const isDesktop = window.innerWidth > 1024
 const isTablet = window.innerWidth <= 1024 && window.innerWidth > 748
+const fruits = ['kiwi', 'banana', 'tomato']
+const fruitsMap = {
+  kiwi: 'Super kiwi',
+  banana: 'Super banana',
+  tomato: 'Super Tomato',
+}
 </script>
 
 <template>
@@ -14,4 +20,8 @@ const isTablet = window.innerWidth <= 1024 && window.innerWidth > 748
     <div v-show="!isDesktop && !isTablet">this is mobile</div>
   </template>
   <div v-show="!isDesktop && !isTablet">this is mobile</div>
+  <div :key="fruit" v-for="(fruit, index) in fruits">{{ index }}: {{ fruit }}</div>
+  <div :key="fruit" v-for="(fruit, key) in fruitsMap">{{ key }}: {{ fruit }}</div>
+  <div :key="val" v-for="val in 5">{{ val }}</div>
+  <div :key="char" v-for="char in 'helllo'">{{ char }}</div>
 </template>
