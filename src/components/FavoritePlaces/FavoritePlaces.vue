650a78f614902aebd734a0bd5cd5c3f295d180f8
Dmytro Chushko <dmytro.chushko@gmail.com>
2025-05-05 00:28:15 +0300

    Add events

--- src/components/FavoritePlaces/FavoritePlaces.vue
+++ src/components/FavoritePlaces/FavoritePlaces.vue
@@ -3,6 +3,7 @@ Original file line numberDiff line numberDiff line change @@ -0,0 +1,12 @@
 import FavoritePlace from '../FavoritePlace/FavoritePlace.vue'
 import IButton from '../IButton/IButton.vue'
 import { ref, reactive } from 'vue'
+import IInput from '../IInput/IInput.vue'
 
 const counter = ref(0) // { value: 0 }
 const user = ref({ name: 'Tom', age: 14 })
@@ -28,11 +29,19 @@ const changeButtonVariant = () => {
 </script>
 
 <template>
-  <div class="px-6">
+  <div
+    class="px-6"
+    @click.self.once="() => console.log('click1')"
+    @keydown.alt.enter="() => console.log('alt pressed')"
+  >
     <div class="text-gray">User name: {{ user.name }}</div>
     <div class="text-gray">Super User name: {{ name }}</div>
     <div class="text-gray mb-4">Додані маркери ({{ counter }})</div>
-    <div class="text-gray mb-4">Додані маркери</div>
+    <div class="text-gray mb-4" @click.stop="() => console.log('click2')">Додані маркери</div>
+    <div class="py-5">
+      <IInput label="Some label" />
+      <a href="/" class="text-black" @click.prevent="() => console.log('prevented')">CLick me</a>
+    </div>
     <slot name="label"></slot>
     <slot name="list">
       <FavoritePlace v-for="n in 4" :key="n" />
