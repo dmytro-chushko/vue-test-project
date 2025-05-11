30c0d5c7cf6d80dd8041d32a6692a2952349f83b
Dmytro Chushko <dmytro.chushko@gmail.com>
2025-05-12 01:10:03 +0300

    Add login script

--- src/components/Auth/LoginForm/LoginForm.vue
+++ src/components/Auth/LoginForm/LoginForm.vue
@@ -3,6 +3,13 @@ import { reactive, watch } from 'vue'
 import IInput from '../../IInput/IInput.vue'
 import IButton from '../../IButton/IButton.vue'
 
+const props = defineProps({
+  isLoading: {
+    default: false,
+    type: Boolean,
+  },
+})
+
 const emit = defineEmits(['submit'])
 const userData = reactive({
   email: '',
@@ -22,6 +29,8 @@ watch(
   <form @submit.prevent="emit('submit', userData)">
     <IInput class="mb-4" label="Електронна пошта" v-model="userData.email" />
     <IInput type="password" label="Пароль" v-model="userData.password" />
-    <IButton class="mt-10 w-full" variant="gradient" type="submit">Увійти </IButton>
+    <IButton class="mt-10 w-full" variant="gradient" type="submit" :is-loading="props.isLoading">
+      Увійти
+    </IButton>
   </form>
 </template>
