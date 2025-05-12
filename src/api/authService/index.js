610cacad0100ee26952f86763a5288f72c4d4433
Dmytro Chushko <dmytro.chushko@gmail.com>
2025-05-13 02:01:15 +0300

    Add refresh token

--- src/api/authService/index.js
+++ src/api/authService/index.js
@@ -1,4 +1,5 @@
 import { clientFetch } from '../clientFetch'
+import { router } from '../../router'
 
 export const TOKEN_KEY = 'token'
 
@@ -9,9 +10,12 @@ class AuthService {
     return Boolean(this.#token)
   }
 
+  getToken() {
+    return this.#token
+  }
+
   setToken(token) {
     localStorage.setItem(TOKEN_KEY, token)
-    clientFetch.defaults.headers.common = { Authorization: `Bearer ${token}` }
     this.#token = token
   }
 
@@ -42,8 +46,42 @@ class AuthService {
   }
 
   async refresh() {
-    return clientFetch.get('/user/refresh')
+    const { data } = await clientFetch.get('/user/refresh')
+    const { accessToken } = data
+
+    this.setToken(accessToken)
   }
 }
 
 export const authService = new AuthService()
+
+clientFetch.interceptors.request.use((request) => {
+  const token = authService.getToken()
+
+  if (token) {
+    request.headers = {
+      ...request.headers,
+      Authorization: `Bearer ${token}`,
+    }
+  }
+
+  return request
+})
+
+clientFetch.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const errorCode = error.response.status
+
+    if (errorCode === 401) {
+      try {
+        return await authService.refresh()
+      } catch (e) {
+        router.push('/auth/login')
+        return Promise.reject(e)
+      }
+    }
+
+    return Promise.reject(error)
+  },
+)
